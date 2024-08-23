61f1ed3c1b13c694e38a91c437daae547bf00f0f
Chan Cheng Leong <u8003980@anu.edu.au>
2024-08-23 19:08:25 +1000

    change styling

--- frontend/src/EventPage.js
+++ frontend/src/EventPage.js
@@ -1,39 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function EventPage() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const fetchData = async () => {
-        axios.get('http://127.0.0.1:5000/responses')
-        .then(response => {
-            console.log(response.data.data);
-            setData(response.data.data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-        
-    };
+  const fetchData = async () => {
+    axios
+      .get("http://127.0.0.1:5000/responses")
+      .then((response) => {
+        console.log(response.data.data);
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
 
-    return (
-        <div>
-            <h1>Event Page</h1>
-            <ul>
-                <button onClick={fetchData} className="border">Fetch Data</button>
-                {data.map((item, index) => (
-                    <li key={index}>
-                        <div>First Name: {item['First Name']}</div>
-                        <div>Last Name: {item['Last Name']}</div>
-                        <div>Role: {item['Role']}</div>
-                        <div>Email Address: {item['Email Address']}</div>
-                        <div>Phone Number: {item['Phone Number']}</div>
-                        <div>Gender: {item['Gender']}</div>
-                        <div>Ethnicity: {item['Ethnicity']}</div>
-                        <div>Age: {item['Age']}</div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Event Page</h1>
+      <button onClick={fetchData} className="border">
+          Fetch Data
+        </button>
+      <ul className="flex flex-col gap-y-4">
+        {data.map((item, index) => (
+          <li key={index} className="border">
+            <div>First Name: {item["First Name"]}</div>
+            <div>Last Name: {item["Last Name"]}</div>
+            <div>Role: {item["Role"]}</div>
+            <div>Email Address: {item["Email Address"]}</div>
+            <div>Phone Number: {item["Phone Number"]}</div>
+            <div>Gender: {item["Gender"]}</div>
+            <div>Ethnicity: {item["Ethnicity"]}</div>
+            <div>Age: {item["Age"]}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
