3178054366356418e46aae34b46eaa5cdee039f2
Chan Cheng Leong <u8003980@anu.edu.au>
2024-08-23 19:02:03 +1000

    update to function

--- backend/server.py
+++ backend/server.py
@@ -17,7 +17,8 @@ def healthcheck():
 # API Route to get data from Google Sheets
 @app.route('/responses')
 def responses():
-    return {'data': get_sheet_values()}
+    sheet_id = '1ZkvjH4i6U8h2iGLI711dwio1VN5FtAxqbO40EWjEtuo'
+    return {'data': get_sheet_values(sheet_id)}
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/sheets_get_values.py
+++ backend/sheets_get_values.py
@@ -3,13 +3,10 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import pandas as pd
 
-sheet_id = '1ZkvjH4i6U8h2iGLI711dwio1VN5FtAxqbO40EWjEtuo'
-sheet_name = "Data"
-
 # gsheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-gsheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
-def get_sheet_values():
+def get_sheet_values(sheet_id):
+    gsheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
     df = pd.read_csv(gsheet_url)
     data_dict = df.to_dict(orient='records')
     print(data_dict)
