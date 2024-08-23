45de9838f7f8f52f5e135ad05fc8387f732a7086
Chan Cheng Leong <u8003980@anu.edu.au>
2024-08-24 08:42:11 +1000

    comment out get_responses

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from sheets_get_values import get_sheet_values
-from get_responses import get_responses
+# from get_responses import get_responses
 
 app = Flask(__name__)
 CORS(app)
@@ -18,9 +18,9 @@ def healthcheck():
 # API Route to get data from Google Sheets
 @app.route('/responses')
 def responses():
-    # sheet_id = '1ZkvjH4i6U8h2iGLI711dwio1VN5FtAxqbO40EWjEtuo'
-    # return {'data': get_sheet_values(sheet_id)}
-    return {'data': get_responses()}
+    sheet_id = '1ZkvjH4i6U8h2iGLI711dwio1VN5FtAxqbO40EWjEtuo'
+    return {'data': get_sheet_values(sheet_id)}
+    # return {'data': get_responses()}
 
 if __name__ == "__main__":
     app.run(debug=True)
