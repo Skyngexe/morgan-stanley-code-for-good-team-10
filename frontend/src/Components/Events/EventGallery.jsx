22e7db4459af6d06bb365e8bf69dc4d7927fd6c4
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2024-08-24 18:41:02 +0800

    limit past and upcoming event display to 4

--- frontend/src/Components/Events/EventGallery.jsx
+++ frontend/src/Components/Events/EventGallery.jsx
@@ -3,14 +3,23 @@ import axios from 'axios';
 import { Container } from '@mui/material';
 import EventCard from './EventCard'; 
 
-function EventGallery() {
+function EventGallery(data) {
   const [eventDataList, setEventDataList] = useState([]);
+  const [upcomingEventsList, setUpcomingEventsList] = useState([]);
+  const [pastEventsList, setPastEventsList] = useState([]);
 
   const fetchData = async () => {
     try {
       const response = await axios.get('http://127.0.0.1:5000/events');
-      console.log(response.data);
-      setEventDataList(response.data);
+      const events = response.data;
+      const today = new Date();
+
+      const upcomingEvents = events.filter(event => new Date(event.startDate) > today);
+      const pastEvents = events.filter(event => new Date(event.endDate) < today);
+
+      setEventDataList(events);
+      setUpcomingEventsList(upcomingEvents);
+      setPastEventsList(pastEvents);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -22,12 +31,26 @@ function EventGallery() {
 
 return (
     <Container maxWidth="xl">
-        <div>
+        <div className='mb-10'>
             <h2 className="text-3xl font-light mb-4">Upcoming Events</h2>
             <hr className="mb-6"/>
-            {eventDataList && eventDataList.length > 0 ? (
+            {upcomingEventsList && upcomingEventsList.length > 0 ? (
                 <div className="grid grid-cols-4 gap-6">
-                    {eventDataList.map((event, index) => (
+                    {upcomingEventsList.map((event, index) => (
+                        <EventCard key={index} event={event} />
+                    ))}
+                </div>
+            ) : (
+                <div>No events found.</div>
+            )}
+
+        </div>
+        <div>
+            <h2 className="text-3xl font-light mb-4">Past Events</h2>
+            <hr className="mb-6"/>
+            {pastEventsList && pastEventsList.length > 0 ? (
+                <div className="grid grid-cols-4 gap-6">
+                    {pastEventsList.slice(0, 4).map((event, index) => (
                         <EventCard key={index} event={event} />
                     ))}
                 </div>
