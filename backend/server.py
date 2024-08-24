0e104543f095bb7cde022a6ded66fc30d3e5c818
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2024-08-24 14:23:41 +0800

    display event gallery to frontend with data from db

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def default():
@@ -10,6 +13,17 @@ def default():
 @app.route('/healthcheck')
 def healthcheck():
     return 'Server is up and running!'
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://codeforgood2024team10:DevL8aYJXQsTm9@cluster0.acjuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client['Event']
+events_collection = db['Event Data']
+
+# API Route to get events
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = list(events_collection.find({}, {'_id': 0})) 
+    return jsonify(events)
 
 if __name__ == "__main__":
     app.run(debug=True)
