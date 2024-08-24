b1a6b1800b3c35b2c0edbd839a05265144057213
Sky Ng <skyng200424@gmail.com>
2024-08-24 13:44:58 +0800

    Documentation and fixing typos

--- backend/server.py
+++ backend/server.py
@@ -8,8 +8,9 @@ from httplib2 import Http
 from oauth2client import client, file, tools
 
 app = Flask(__name__)
-MONGO_URI = "mongodb+srv://codeforgood2024team10:DevL8aYJXQsTm9@cluster0.acjuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+
+MONGO_URI = "mongodb+srv://codeforgood2024team10:DevL8aYJXQsTm9@cluster0.acjuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 EVENT_DB = "Event"
 USER_DB = 'User'
 
@@ -22,9 +23,18 @@ event_data = event_db['Event Data']
 user_data = event_db['User Data']
 
 class UserRole(Enum):
+    """
+        Enum representing the roles of users.
+
+    Attributes:
+        VOLUNTEER: Represents a volunteer user.
+        ADMIN: Represents an admin user.
+        PARTICIPANT: Represents a participant user.
+    """
+
     VOLUNTEER = "volunteer"
     ADMIN = "admin"
-    MODERATOR = "moderator"
+    PARTICIPANT = "participant"
 
 @app.route('/')
 def default():
@@ -32,6 +42,38 @@ def default():
 
 @app.route('/write/user', methods=['POST'])
 def create_new_user():
+    """
+    Create a new user and insert the user data into the database.
+
+    Expects JSON data in the request body with the following structure:
+    
+    {
+        "username": "Jane123",
+        "password": "Janeaccount",
+        "email": "jane@abc.com",
+        "number": "62018209",
+        "role": "volunteer",
+        "ethnicity": "American",
+        "gender": "Female",
+        "age": {
+            "$numberInt": "20"
+        },
+        "preferred_language": "english",
+        "points": {
+            "$numberInt": "0"
+        }
+    }
+
+    Returns:
+        dict: A dictionary containing a success message if the user data is inserted successfully.
+
+    Raises:
+        400: If there are any errors during the user creation process.
+
+    Notes:
+        - The function expects JSON data in the request body with the following fields:
+            - "role": The role of the user. Allowed values are 'volunteer', 'admin', or 'participants'.
+    """
     try:
         new_user = request.json  # Access JSON data from the request object
         
@@ -46,6 +88,35 @@ def create_new_user():
 
 @app.route('/write/event', methods=['POST'])
 def create_new_event():
+    """
+    Create a new event and insert the event data into the database.
+
+    Expects JSON data in the request body with the following structure:
+    
+    {
+        "name": "Weekly Chai Gathering for EM Women",
+        "description": "A Weekly gathering for Women, join us if interested!",
+        "imageURL": "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/453646501_505226555…",
+        "startDate": "24 August 2024",
+        "endDate": "20 December 2024",
+        "volunteer_Quota": 5,
+        "participant_Quota": 20,
+        "eventType": "women",
+        "videoURL": "https://www.youtube.com/watch?v=SlDJUL7lMCk",
+        "status": "full",
+        "location": "Singapore",
+        "fee": "free"
+    }
+
+    Returns:
+        dict: A dictionary containing a success message if the event data is inserted successfully.
+
+    Raises:
+        400: If there are any errors during the event creation process.
+
+    Notes:
+        - Make sure to provide the required fields in the JSON data for the new event.
+    """
     try:
         new_event = request.json  # Access JSON data from the request object
         event_data.insert_one(new_event)
@@ -123,26 +194,7 @@ def transform_event_data_to_feedback_questions():
                 "location": {"index": 1},
             }
             })
-            '''
-            questions.append({
-                "createItem": {
-                    "item": {
-                        "questionItem": {
-                            "question": {
-                                "title": "What suggestions do you have for improvement?",
-                                "questionItem": {
-                                    "textQuestion": {
-                                        "paragraph": True
-                                    },
-                                    "required": False
-                                }
-                            }
-                        },
-                        "title": f"Feedback for {event['name']}"
-                    }
-                }
-            })
-        '''
+
         return {"requests": questions}
 
 @app.route('/create/form', methods=['POST'])
@@ -167,7 +219,7 @@ def create_gform():
     # Request body for creating a form
     NEW_FORM = {
         "info": {
-            "title": "Quickstart form",
+            "title": "Event feeback form",
         }
     }
 
@@ -183,7 +235,6 @@ def create_gform():
 
     # Prints the result to show the question has been added
     get_result = form_service.forms().get(formId=result["formId"]).execute()
-    print(get_result)
     return get_result
 
 def get_responses_with_formId(formId): 
