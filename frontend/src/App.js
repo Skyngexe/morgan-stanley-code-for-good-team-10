86b3cdc93aaa7401ed621a4b3797b162584cb22f
Chan Cheng Leong <u8003980@anu.edu.au>
2024-08-25 03:00:09 +1000

    trying to use react query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,8 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-  SignIn,
-} from "@clerk/clerk-react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 import AdminPage from "./Pages/AdminPage";
 import Header from "./Components/Header";
@@ -19,53 +14,82 @@ import CreateAccountPage from "./Pages/CreateAccountPage";
 import useStore from "./Components/secureStore";
 
 function App() {
-  const email = useStore((state) => state.email);
-  const imageUrl = useStore((state) => state.imageUrl);
-  const accountCreated = useStore((state) => state.accountCreated);
+  const googleId = useStore((state) => state.googleId);
 
-  const SignedIn = ({ children }) => {
-    if (email && accountCreated) {
-      return <>{children}</>;
-    }
-    return null;
-  };
+  const { data, isLoading, error } = useQuery(["user", googleId], () =>
+    axios
+      .get(`http://127.0.0.1:5000/read/user/${googleId}`)
+      .then((res) => res.data)
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        return <div>Server Error</div>;
+      }),
+      {
+        enabled: !!googleId, // Only run the query if googleId is available
+      }
+  );
 
-  const CreateAccount = ({ children }) => {
-    if (email && !accountCreated) {
-      return <>{children}</>;
-    }
-    return null;
-  };
+  if (!googleId) {
+    return <LoginPage />;
+  }
 
-  const SignedOut = ({ children }) => {
-    if (!email) {
-      return <>{children}</>;
-    }
-    return null;
-  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data.message === "User not found") {
+    return <CreateAccountPage />;
+  } else if (data.message !== "User found") {
+    return <div>Server Error</div>;
+  }
+
+  // const email = useStore((state) => state.email);
+  // const imageUrl = useStore((state) => state.imageUrl);
+  // const accountCreated = useStore((state) => state.accountCreated);
+
+  // const SignedOut = ({ children }) => {
+  //   if (!email) {
+  //     return <>{children}</>;
+  //   }
+  //   return null;
+  // };
+
+  // const CreateAccount = ({ children }) => {
+  //   if (email && !accountCreated) {
+  //     return <>{children}</>;
+  //   }
+  //   return null;
+  // };
+
+  // const SignedIn = ({ children }) => {
+  //   if (email && accountCreated) {
+  //     return <>{children}</>;
+  //   }
+  //   return null;
+  // };
 
   return (
-    <header>
-      <SignedOut>
-        <LoginPage />
-      </SignedOut>
-      <CreateAccount>
-        <CreateAccountPage />
-      </CreateAccount>
-      <SignedIn>
-        <Router>
-          <div className="App">
-            <Header />
-            <Routes>
-              <Route path="/" element={<EventPage />} />
-              <Route path="/events" element={<EventPage />} />
-              <Route path="/dashboard" element={<p>Dashboard Page</p>} />
-              <Route path="/admin" element={<AdminPage />} />
-            </Routes>
-          </div>
-        </Router>
-      </SignedIn>
-    </header>
+    // <header>
+    //   <SignedOut>
+    //     <LoginPage />
+    //   </SignedOut>
+    //   <CreateAccount>
+    //     <CreateAccountPage />
+    //   </CreateAccount>
+    //   <SignedIn>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<EventPage />} />
+          <Route path="/events" element={<EventPage />} />
+          <Route path="/dashboard" element={<p>Dashboard Page</p>} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </div>
+    </Router>
+    //   </SignedIn>
+    // </header>
   );
 }
 
