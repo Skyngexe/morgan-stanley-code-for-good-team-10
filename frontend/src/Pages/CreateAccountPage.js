15007b29226fbc67d11613dae60970ead4514209
Chan Cheng Leong <u8003980@anu.edu.au>
2024-08-25 13:27:46 +1000

    change styling

--- frontend/src/Pages/CreateAccountPage.js
+++ frontend/src/Pages/CreateAccountPage.js
@@ -47,94 +47,105 @@ function CreateAccountPage() {
   };
 
   return (
-    <div>
-      <h1>Creating account for {email}</h1>
-      <form className="flex flex-col" onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="username"
-            value={accountData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Number:{" "}
-          <input
-            type="text"
-            name="number"
-            value={accountData.number}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Role:{" "}
-          <input
-            type="radio"
-            name="role"
-            value="volunteer"
-            checked={accountData.role === "volunteer"}
-            onChange={handleChange}
-          />{" "}
-          Volunteer
-          <input
-            type="radio"
-            name="role"
-            value="participant"
-            checked={accountData.role === "participant"}
-            onChange={handleChange}
-          />{" "}
-          Participant
-        </label>
-        <label>
-          Ethnicity:{" "}
-          <input
-            type="text"
-            name="ethnicity"
-            value={accountData.ethnicity}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Gender:{" "}
-          <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={accountData.gender === "male"}
-            onChange={handleChange}
-          />{" "}
-          Male
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-            checked={accountData.gender === "female"}
-            onChange={handleChange}
-          />{" "}
-          Female
-        </label>
-        <label>
-          Age:{" "}
-          <input
-            type="number"
-            name="age"
-            value={accountData.age}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Preferred Language:{" "}
-          <input
-            type="text"
-            name="preferred_language"
-            value={accountData.preferred_language}
-            onChange={handleChange}
-          />
-        </label>
-        <input type="submit" value="Create Account" />
-      </form>
+    <div className="flex flex-col items-center">
+      <div className="mt-32 container min-w-[300px] shadow-xl">
+        <h1>Creating account for {email}</h1>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <label>
+            Username:{" "}
+            <input
+              type="text"
+              name="username"
+              value={accountData.username}
+              onChange={handleChange}
+              className="input input-bordered"
+            />
+          </label>
+          <label>
+            Number:{" "}
+            <input
+              type="text"
+              name="number"
+              value={accountData.number}
+              onChange={handleChange}
+              className="input input-bordered"
+            />
+          </label>
+          <label>
+            Role:{" "}
+            <input
+              type="radio"
+              name="role"
+              value="volunteer"
+              checked={accountData.role === "volunteer"}
+              onChange={handleChange}
+              className="input input-bordered"
+            />{" "}
+            Volunteer
+            <input
+              type="radio"
+              name="role"
+              value="participant"
+              checked={accountData.role === "participant"}
+              onChange={handleChange}
+              className="input input-bordered"
+            />{" "}
+            Participant
+          </label>
+          <label>
+            Ethnicity:{" "}
+            <input
+              type="text"
+              name="ethnicity"
+              value={accountData.ethnicity}
+              onChange={handleChange}
+              className="input input-bordered"
+            />
+          </label>
+          <label>
+            Gender:{" "}
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={accountData.gender === "male"}
+              onChange={handleChange}
+              className="input input-bordered"
+            />{" "}
+            Male
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={accountData.gender === "female"}
+              onChange={handleChange}
+              className="input input-bordered"
+            />{" "}
+            Female
+          </label>
+          <label>
+            Age:{" "}
+            <input
+              type="number"
+              name="age"
+              value={accountData.age}
+              onChange={handleChange}
+              className="input input-bordered"
+            />
+          </label>
+          <label>
+            Preferred Language:{" "}
+            <input
+              type="text"
+              name="preferred_language"
+              value={accountData.preferred_language}
+              onChange={handleChange}
+              className="input input-bordered"
+            />
+          </label>
+          <input type="submit" value="Create Account" />
+        </form>
+      </div>
     </div>
   );
 }
